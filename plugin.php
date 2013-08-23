79a39be94eeb1ea2a3d4b8ac31e12c5cb785af94
Brad Daily <brad@slideshowpro.net>
2013-08-23 16:42:49 -0500

    Testing setup for count integration

--- plugin.php
+++ plugin.php
@@ -7,23 +7,37 @@ class KokenDisqus extends KokenPlugin {
 		$this->require_setup = true;
 		$this->register_hook('before_closing_body', 'render_js');
 		$this->register_hook('discussion', 'render_div');
+		$this->register_hook('discussion_count', 'render_count_div');
 	}
 
-	function render_div()
+	function render_div($item)
 	{
-		echo '<div id="disqus_thread"></div>';
+		echo '<script>var disqus_identifier = "koken-disqus-' . $item['id'] . '";</script><div id="disqus_thread"></div>';
+
+	}
+
+	function render_count_div($item)
+	{
+		echo '<a href="' . $item['url'] . '#disqus_thread" data-disqus-identifier="koken-disqus-' . $item['id'] . '"></a>';
 	}
 
 	function render_js()
 	{
 		echo <<<OUT
 <script type="text/javascript">
+	var disqus_shortname = '{$this->data->shortname}';
 	(function() {
 		var disqus = function() {
 			if ($('#disqus_thread').length) {
 				(function() {
 					var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-					dsq.src = 'http://{$this->data->shortname}.disqus.com/embed.js';
+					dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+					(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+				})();
+			} else {
+				(function() {
+					var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+					dsq.src = '//' + disqus_shortname + '.disqus.com/count.js';
 					(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 				})();
 			}
